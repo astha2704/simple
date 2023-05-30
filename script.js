60f686cc5aed1ae8b33a401304dc181530d19cc6
astha2704 <135014532+astha2704@users.noreply.github.com>
2023-05-30 17:02:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function apologyPrompt() {
+    var apology = prompt("Please enter your apology:", "I'm sorry!");
+
+    if (apology) {
+        alert("i love you Thank you.");
+    } else {
+        alert("plzz");
+    }
+}
